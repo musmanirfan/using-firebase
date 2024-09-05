87cb793ac13d74b75dbd6cc35decfcdcfce6753e
musmanirfan <usmanmala356@gmail.com>
2024-09-06 04:52:58 +0500

    first commit

--- src/app/welcomePage/page.tsx
+++ src/app/welcomePage/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+
+
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function WelcomePage() {
-  return (
-    <h1>Welcome to our website</h1>
-  )
+    const router = useRouter()
+
+    const logOutFun = () => {
+        router.push('/')
+    }
+    return (
+
+
+        <>
+            <h1>Welcome to our website</h1>
+            <button onClick={logOutFun}>Logout</button>
+        </>
+    )
 }
