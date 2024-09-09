1ec5106d82d9d0164b14de346d5c7e74e76867f1
musmanirfan <usmanmala356@gmail.com>
2024-09-09 21:58:10 +0500

    bug fixing in building

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-
+"use client"
+import { useEffect } from "react";
 import SignUp from "./signup";
 
 export default function Home() {
-  
+
+  useEffect(() => console.log(process.env.NEXT_PUBLIC_apiKey), [])
+
   return (
     <div className="mx-auto">
       <SignUp />
