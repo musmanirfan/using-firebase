9adeb6e81faad7e2c355848a15f205564798a288
musmanirfan <usmanmala356@gmail.com>
2024-09-09 22:09:48 +0500

    bug fixing in building

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import SignUp from "./signup";
 
 export default function Home() {
 
-  useEffect(() => console.log(process.env.NEXT_PUBLIC_apiKey), [])
+  useEffect(() => {
+    console.log("apiKey => ", process.env.NEXT_PUBLIC_apiKey)
+    console.log("authDomain => ", process.env.NEXT_PUBLIC_authDomain)
+    console.log("NEXT_PUBLIC_projectId => ", process.env.NEXT_PUBLIC_projectId)
+    console.log("NEXT_PUBLIC_storageBucket => ", process.env.NEXT_PUBLIC_storageBucket)
+    console.log("NEXT_PUBLIC_messagingSenderId => ", process.env.NEXT_PUBLIC_messagingSenderId)
+    console.log("NEXT_PUBLIC_appId => ", process.env.NEXT_PUBLIC_appId)
+    console.log("NEXT_PUBLIC_measurementId => ", process.env.NEXT_PUBLIC_measurementId)
+  }, [])
 
   return (
     <div className="mx-auto">
